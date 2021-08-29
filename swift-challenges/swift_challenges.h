3ca6cd8487dbae57b58458852dc733c992a6b7e6
Mark O'Hara <mohara20@qub.ac.uk>
2021-08-29 14:50:19 +0100

    base project

---
+++ swift-challenges/swift_challenges.h
@@ -0,0 +1,18 @@
+//
+//  swift_challenges.h
+//  swift-challenges
+//
+//  Created by Mark O'Hara on 29/08/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for swift_challenges.
+FOUNDATION_EXPORT double swift_challengesVersionNumber;
+
+//! Project version string for swift_challenges.
+FOUNDATION_EXPORT const unsigned char swift_challengesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <swift_challenges/PublicHeader.h>
+
+
